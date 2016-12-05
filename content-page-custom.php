183b37d26575de324cc9918c71718b816b5bc55a
Matt Radford <webform@mattrad.co.uk>
2016-12-05 21:34:41 +0000

    ACF page sections

--- content-page-custom.php
+++ content-page-custom.php
@@ -11,5 +11,21 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php generate_article_schema( 'CreativeWork' ); ?>>
-	acf fields to go here
+  <?php
+  include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+  if ( is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
+
+    if(have_rows('page_sections')) {
+
+      while ( have_rows('page_sections') ) {
+
+        the_row();
+        get_template_part( 'page-sections/content', get_row_layout() );
+
+      }
+
+    } // end of if content_row
+  } // end "is ACF enabled?"
+  ?>
 </article>
