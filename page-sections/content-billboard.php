679d0c66314044a05fd356ea8cffbffe88d46ab8
Matt Radford <webform@mattrad.co.uk>
2016-12-05 22:12:40 +0000

    oembed

--- page-sections/content-billboard.php
+++ page-sections/content-billboard.php
@@ -1,12 +1,17 @@
 <section class="billboard">
 
+	<?php
+  if( get_sub_field( 'video' ) ) {
+    the_sub_field( 'video' );
+  }
+  ?>
+
   <?php
   $image = get_sub_field( 'logo' );
-  if ( !empty( $image ) ) {
-    print_r($image);
+  if ( ! empty( $image ) ) {
     $image_id = $image['id'];
     echo wp_get_attachment_image( $image_id, 'full' );
+  }
   ?>
-  <?php } ?>
 
 </section>
