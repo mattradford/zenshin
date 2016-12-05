f5338f8814ca6b325239f0c960cc5d4818e6cd96
Matt Radford <webform@mattrad.co.uk>
2016-12-05 21:38:12 +0000

    GP actions back in to template

--- page-custom.php
+++ page-custom.php
@@ -11,15 +11,17 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 get_header(); ?>
 
-	<div id="primary">
-		<main id="main">
+<div id="primary" <?php generate_content_class();?>>
+  <main id="main" <?php generate_main_class(); ?>>
+      <?php do_action('generate_before_main_content'); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+  			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page-custom' ); ?>
+  				<?php get_template_part( 'content', 'page-custom' ); ?>
 
-			<?php endwhile; ?>
-
+  			<?php endwhile; ?>
+        
+      <?php do_action('generate_after_main_content'); ?>
 		</main>
 	</div>
 
