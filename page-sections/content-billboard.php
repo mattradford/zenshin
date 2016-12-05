923b3e456b0d11478f33eef68270a7349de40dd1
Matt Radford <webform@mattrad.co.uk>
2016-12-05 22:02:25 +0000

    And again

--- page-sections/content-billboard.php
+++ page-sections/content-billboard.php
@@ -4,8 +4,8 @@
   $image = get_sub_field( 'logo' );
   if ( !empty( $image ) ) {
     print_r($image);
-    $image_id = $image['ID'];
-    echo wp_get_attachment( $image_id, 'full' );
+    $image_id = $image[ID];
+    echo wp_get_attachment_image( $image_id, 'full' );
   ?>
   <?php } ?>
 
