72872eae6d267e25b4f2e93f311a52c83648160f
Matt Radford <webform@mattrad.co.uk>
2016-12-05 21:40:22 +0000

    Hmm

--- page-custom.php
+++ page-custom.php
@@ -1,9 +1,13 @@
 <?php
 /**
- * Custom template for displaying ACF fields.
+ * The template for displaying all pages.
  *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
  *
- * @package zenshin
+ * @package GeneratePress
  */
 
 // No direct access, please
@@ -11,19 +15,19 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 get_header(); ?>
 
-<div id="primary" <?php generate_content_class();?>>
-  <main id="main" <?php generate_main_class(); ?>>
-      <?php do_action('generate_before_main_content'); ?>
+	<div id="primary" <?php generate_content_class();?>>
+		<main id="main" <?php generate_main_class(); ?>>
+			<?php do_action('generate_before_main_content'); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-  			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
 
-  				<?php get_template_part( 'content', 'page-custom' ); ?>
 
-  			<?php endwhile; ?>
-        
-      <?php do_action('generate_after_main_content'); ?>
-		</main>
-	</div>
+			<?php endwhile; // end of the loop. ?>
+			<?php do_action('generate_after_main_content'); ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php
+do_action('generate_sidebars');
 get_footer();
