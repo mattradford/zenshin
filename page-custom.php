131b3db3916b8d0e3fbcddb80dcd26b7ba518620
Matt Radford <webform@mattrad.co.uk>
2016-12-05 21:44:17 +0000

    Custom page strangeness

--- page-custom.php
+++ page-custom.php
@@ -2,6 +2,8 @@
 /**
  * The template for displaying all pages.
  *
+ * Template Name: Custom Page
+ *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site will use a
